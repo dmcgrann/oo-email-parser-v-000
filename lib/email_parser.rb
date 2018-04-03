8501e7ae9606ced5357653ec1e9f70c7945e9c56
David McGrann <dpmcgrann@gmail.com>
2018-04-03 01:57:35 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,19 +1,9 @@
 class EmailParser
-  attr_accessor :email, :parse
+  attr_accessor :email, :address
 
   def initialize(email)
-    self
+    @email
   end
 
-  def self.parse(csv_data)
-    rows = csv_data.split("\n")
-    parser = rows.collect do |row|
-      data = row.split(", ")
-      email = data
-
-      address = self.new # This is an important line.
-      address.email = address
-      address
-    end
   end
 end
