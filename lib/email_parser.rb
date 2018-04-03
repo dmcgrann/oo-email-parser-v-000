c580a0f80cbff59300e27c19382a75b1db4cd056
David McGrann <dpmcgrann@gmail.com>
2018-04-03 01:25:16 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,13 +1,13 @@
 class EmailParser
   attr_accessor :email, :parse
-  @@rows = []
+  
   def initialize(email)
     self
   end
 
   def self.parse(list)
     @@rows = list.split("\n")
-    parser = rows.collect {|x| x.email == x}
+    parser = rows.select {|x| x.email == x}
     email
   end
 end
